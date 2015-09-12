abc79324c3bff5e6fe64265f9df29838f0feb0b0
Vladimir Dmitriev <defurator@mail.ru>
2015-09-12 23:53:12 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="UTF-8">
+  <title>Iphone - Главная</title>
+  <link rel="stylesheet" href="css/all.css" type="text/css" />
+  <link rel="stylesheet" href="css/animate.css" type="text/css"/>
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/all-old-ie.css">
+  <![endif]-->
+  <!--[if gt IE 8]>
+    <link rel="stylesheet" href="css/all.css">
+  <![endif]-->
+  <script src="//maps.googleapis.com/maps/api/js?v=3.exp"></script>
+  <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/bg-loaded.js"></script>
+  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+  <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+  <script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
+  <script type="text/javascript" src="js/jquery.waypoints.min.js"></script>
+  <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>  
+</head>
+<body>
+  <header class="header">
+    
+  </header>
+
+    
+
+  <footer class="footer">
+    
+  </footer>
+</body>
+</html>
